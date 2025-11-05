399d54720b5f4f3802d57bcb7bdfc48d9ddb278d
SKYDESTROY <zhangtengfool1996@gmail.com>
2025-11-05 20:21:01 +0800

    状态机

--- player.gd
+++ player.gd
@@ -1,4 +1,13 @@
 extends CharacterBody2D
+
+enum  State {
+	Idle,
+	Running,
+	Jump,
+	Falling
+}
+const groundstates := [State.Idle,State.Running]
+
 const RUN_SPEED :=130.0
 const FLOOR_ACCELERATION:= RUN_SPEED/0.2
 const AIR_ACCELERATION:= RUN_SPEED/0.02
@@ -9,7 +18,7 @@ var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 #预输入跳跃，接触地面立即无延迟跳跃
 @onready var jumprequsttimer: Timer = $jumprequsttimer
 #预输入悬崖腾空跳跃，离开悬崖短时间仍可跳跃
-@onready var timer: Timer = $airjumptimer
+@onready var airjumptimer: Timer = $airjumptimer
 
 @onready var sprite_2d: Sprite2D = $Sprite2D 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -24,41 +33,80 @@ func _unhandled_input(event: InputEvent) -> void:
 		if(-velocity.y) > (-LITTLE_JUMP_VELOCITY):
 			velocity.y = JUMP_VELOCITY/2
 
-func _physics_process(delta: float) -> void:
-	#自由态横向纵向速度
+func tickphysics(state:State,delta: float) -> void:
+	#print(state)
+	match state:
+		State.Idle:
+			move(delta)
+		State.Running:
+			move(delta)
+		State.Jump:
+			move(delta)
+		State.Falling:
+			move(delta)
+
+func move(delta: float)	:
+#自由态横向纵向速度
 	var direction :=Input.get_axis("move_left","move_right")
 	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
 	#地面缓慢加速,空中迅速加速
 	velocity.x = move_toward(velocity.x,direction* RUN_SPEED,acceleration * delta)
 	velocity.y += gravity * delta
-	
-	var canjump := is_on_floor() or timer.time_left>0
-	var shouldjump := canjump and jumprequsttimer.time_left>0 #Input.is_action_just_pressed("jump")
-	#跳跃 速度
-	if shouldjump:
-		velocity.y = JUMP_VELOCITY
-		timer.stop()
-		jumprequsttimer.stop()
-	#判断状态
-	if is_on_floor():
-		if is_zero_approx(direction) and is_zero_approx(velocity.x):
-			animation_player.play("idle")
-		else:
-			animation_player.play("running")
-	elif velocity.y < 0 :
-		animation_player.play("jump")
-	else:
-		animation_player.play("falling")
-		
+
 	#贴图跟随状态翻转	
 	if not is_zero_approx(direction):
 		sprite_2d.flip_h = direction < 0
-	#对比移动更新前是否在地面	
-	var wasfloor: = is_on_floor()
 	move_and_slide()
-	if is_on_floor() != wasfloor:
-		if wasfloor	and not shouldjump:
-			timer.start()
-		else :
-			timer.stop()
+	
+func getnextstate(state:State) ->State:
+	var canjump := is_on_floor() or airjumptimer.time_left>0
+	var shouldjump := canjump and jumprequsttimer.time_left>0 #Input.is_action_just_pressed("jump")
+	if shouldjump:
+		return State.Jump
 		
+	var direction :=Input.get_axis("move_left","move_right")
+	var isstill := is_zero_approx(direction) and is_zero_approx(velocity.x)
+	
+	match state:
+		State.Idle:
+			if not isstill:
+				return State.Running
+		State.Running:
+			if isstill:
+				return State.Idle
+			if not is_on_floor():
+				return State.Falling
+		State.Jump:
+			if velocity.y > 0:
+				return State.Falling
+		State.Falling:
+			if is_on_floor() :
+				if isstill:
+					return State.Idle
+				else:
+					return State.Running
+	
+	return state
+	
+func transitionstate(from:State,to:State) :
+	if from not in groundstates and to in groundstates :
+		airjumptimer.stop()
+		
+	match to:
+		State.Idle:
+			animation_player.play("idle")
+			$statedebug.text=str("idle")
+		State.Running:
+			animation_player.play("running")
+			$statedebug.text=str("running")
+		State.Jump:
+			animation_player.play("jump")
+			$statedebug.text=str("jump")
+			velocity.y = JUMP_VELOCITY
+			airjumptimer.stop()
+			jumprequsttimer.stop()			
+		State.Falling:
+			animation_player.play("falling")
+			$statedebug.text=str("falling")
+			if from in groundstates:
+				airjumptimer.start()
---
+++ statemachine.gd
@@ -0,0 +1,23 @@
+class_name statemachine
+extends Node
+
+var currentstate : int :
+	set(v):
+		#由角色传入状态
+		owner.transitionstate(currentstate,v)
+		currentstate = v
+		
+func _ready() -> void:
+	#保证角色准备传入
+	await owner.ready
+	#枚举从0始，-1不影响状态机
+	currentstate = 0
+	
+func _physics_process(delta: float) -> void:
+	while true:
+		var next := owner.getnextstate(currentstate) as int
+		if currentstate == next:
+			break
+		currentstate = next
+	#角色状态运行
+	owner.tickphysics(currentstate,delta)
