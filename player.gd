690e22aa8bbc5d61b16fe7eda21224dab150bc36
SKYDESTROY <zhangtengfool1996@gmail.com>
2025-11-05 21:17:55 +0800

    新增着陆状态

--- player.gd
+++ player.gd
@@ -4,9 +4,11 @@ enum  State {
 	Idle,
 	Running,
 	Jump,
-	Falling
+	Falling,
+	Landing
 }
-const groundstates := [State.Idle,State.Running]
+#利用地面状态判断腾空跳
+const groundstates := [State.Idle,State.Running,State.Landing]
 
 const RUN_SPEED :=130.0
 const FLOOR_ACCELERATION:= RUN_SPEED/0.2
@@ -14,7 +16,9 @@ const AIR_ACCELERATION:= RUN_SPEED/0.02
 #y轴正方向向下
 const JUMP_VELOCITY :=-360.0
 const LITTLE_JUMP_VELOCITY:= JUMP_VELOCITY/2
-var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
+var defaultgravity := ProjectSettings.get("physics/2d/default_gravity") as float
+var isfirsttick := false
+
 #预输入跳跃，接触地面立即无延迟跳跃
 @onready var jumprequsttimer: Timer = $jumprequsttimer
 #预输入悬崖腾空跳跃，离开悬崖短时间仍可跳跃
@@ -34,19 +38,22 @@ func _unhandled_input(event: InputEvent) -> void:
 			velocity.y = JUMP_VELOCITY/2
 
 func tickphysics(state:State,delta: float) -> void:
-	#print(state)
 	match state:
 		State.Idle:
-			move(delta)
+			move(defaultgravity,delta)
 		State.Running:
-			move(delta)
+			move(defaultgravity,delta)
 		State.Jump:
-			move(delta)
+			#起跳第一帧去掉重力影响
+			move(0.0 if isfirsttick else defaultgravity,delta)
 		State.Falling:
-			move(delta)
-
-func move(delta: float)	:
-#自由态横向纵向速度
+			move(defaultgravity,delta)
+		State.Landing:
+			pass
+	isfirsttick = false
+	
+func move(gravity:float,delta: float)	:
+	#自由态横向纵向速度
 	var direction :=Input.get_axis("move_left","move_right")
 	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
 	#地面缓慢加速,空中迅速加速
@@ -58,13 +65,15 @@ func move(delta: float)	:
 		sprite_2d.flip_h = direction < 0
 	move_and_slide()
 	
+	
 func getnextstate(state:State) ->State:
 	var canjump := is_on_floor() or airjumptimer.time_left>0
-	var shouldjump := canjump and jumprequsttimer.time_left>0 #Input.is_action_just_pressed("jump")
+	var shouldjump := canjump and jumprequsttimer.time_left>0 
 	if shouldjump:
 		return State.Jump
 		
 	var direction :=Input.get_axis("move_left","move_right")
+	#地面静止站立状态
 	var isstill := is_zero_approx(direction) and is_zero_approx(velocity.x)
 	
 	match state:
@@ -82,9 +91,12 @@ func getnextstate(state:State) ->State:
 		State.Falling:
 			if is_on_floor() :
 				if isstill:
-					return State.Idle
-				else:
+					return State.Landing
+				else :
 					return State.Running
+		State.Landing:
+			if not animation_player.is_playing():
+				return State.Idle
 	
 	return state
 	
@@ -110,3 +122,7 @@ func transitionstate(from:State,to:State) :
 			$statedebug.text=str("falling")
 			if from in groundstates:
 				airjumptimer.start()
+		State.Landing:
+			animation_player.play("landing")
+			$statedebug.text=str("landing")			
+	isfirsttick =true
